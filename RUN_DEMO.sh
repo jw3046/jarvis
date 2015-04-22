4b20082d2dca51f1d4a877eaa801738df601d661
Edward Li <el2724@columbia.edu>
2015-04-22 00:02:37 -0400

    integrated entire pipeline for testing

--- RUN_DEMO.sh
+++ RUN_DEMO.sh
@@ -2,7 +2,7 @@
 
 cd opendial-1.1/
 
-ant run -Ddomain=domains/test-domain/test-domain.xml -Dmodules=opendial.plugins.ATTSpeech -Dkey=gj0jyoofj3hn3v4stbwtzsuj5dio3ck7 -Dsecret=j9bfoadowuewg1mf87kepyfkso62k31k
+ant run -Ddomain=domains/jarvis-cal/jarvis.xml -Dmodules=opendial.plugins.ATTSpeech -Dkey=gj0jyoofj3hn3v4stbwtzsuj5dio3ck7 -Dsecret=j9bfoadowuewg1mf87kepyfkso62k31k
 
 cd ../
 
