fd699db0035d327532df6c65d6f51a4a8dfcccc0
Edward Li <el2724@columbia.edu>
2015-05-04 15:58:01 -0400

    new demo script

---
+++ DEMO.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd opendial-1.1/
+
+ant run -Ddomain=domains/final-jarvis/jarvis.xml -Dmodules=opendial.plugins.ATTSpeech -Dkey=gj0jyoofj3hn3v4stbwtzsuj5dio3ck7 -Dsecret=j9bfoadowuewg1mf87kepyfkso62k31k
+
+cd ../
+
+
